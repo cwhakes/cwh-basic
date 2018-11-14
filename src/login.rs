d9ea0edcbf1617e4a847c9718b864ef0e78e104a
Will Hakes <info@cwilliamhakes.com>
2018-11-14 16:50:35 -0700

    Handle missing account

--- src/login.rs
+++ src/login.rs
@@ -14,6 +14,20 @@ pub struct Login {
     pub password: String
 }
 
+pub struct Account {
+    pub username: String,
+    pub passhash: String,
+}
+
+impl Account {
+    fn matches(&self, login: &Login) -> Option<bool> {
+        Some(
+            (self.username == login.username) &
+            (verify(&login.password, &self.passhash).ok()?)
+        )
+    }
+}
+
 #[get("/login")]
 pub fn login_page(flash: Option<FlashMessage>) -> Template {
     let mut context = HashMap::new();
@@ -26,14 +40,16 @@ pub fn login_page(flash: Option<FlashMessage>) -> Template {
 
 
 #[post("/login", data = "<login>")]
-pub fn login(mut cookies: Cookies, login: Form<Login>, valid_login: State<(String, String)>) -> Result<Flash<Redirect>, Flash<Redirect>> {
-    if (*login.username == (*valid_login).0) &
-        (verify(&login.password, &(*valid_login).1).expect("Could not hash password"))
-    {
-        cookies.add_private(Cookie::new("user_id", 1.to_string()));
-        Ok(Flash::success(Redirect::to(uri!(login_page)), "Correct username/password."))
+pub fn login(mut cookies: Cookies, login: Form<Login>, valid_login: State<Option<Account>>) -> Result<Flash<Redirect>, Flash<Redirect>> {
+    if let Some(test) = valid_login.as_ref().and_then(|v| v.matches(&login)) {
+        if test {
+            cookies.add_private(Cookie::new("user_id", 1.to_string()));
+            Ok(Flash::success(Redirect::to(uri!(login_page)), "Correct username/password."))
+        } else {
+            Err(Flash::error(Redirect::to(uri!(login_page)), "Invalid username/password."))
+        }
     } else {
-        Err(Flash::error(Redirect::to(uri!(login_page)), "Invalid username/password."))
+        Err(Flash::error(Redirect::to(uri!(login_page)), "Username/password error."))
     }
 }
 
@@ -46,10 +62,15 @@ pub fn logout(mut cookies:Cookies) -> Flash<Redirect> {
 pub fn get_valid_login() -> AdHoc {
     AdHoc::on_attach("Login Credentials", |rocket| {
 
-        let valid_login = (
-            rocket.config().get_str("username").expect("No Username").to_owned(),
-            rocket.config().get_str("passhash").expect("No Passhash").to_owned(),
-        );
+        let valid_login = if let (Ok(username), Ok(passhash)) = (
+            rocket.config().get_str("username"),
+            rocket.config().get_str("passhash"),
+        ) {
+            Some( Account {
+                username: username.to_owned(),
+                passhash: passhash.to_owned(),
+            })
+        } else { None };
 
         Ok(rocket.manage(valid_login))
     })
