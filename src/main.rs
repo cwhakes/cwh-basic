1d80b94cf9f5ab26fc30c8cce3c09a08b5b498e4
Will Hakes <info@cwilliamhakes.com>
2018-11-14 20:19:16 -0700

    Add some basic tests

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ extern crate rocket_contrib;
 
 mod login;
 
+#[cfg(test)]
+mod test;
+
 use rocket::Request;
 use rocket_contrib::serve::{StaticFiles, Options};
 use rocket_contrib::templates::Template;
@@ -17,12 +20,16 @@ fn not_found(req: &Request) -> Template {
     Template::render("error/404", &map)
 }
 
-fn main() {
+fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .attach(Template::fairing())
         .attach(login::get_valid_login())
         .mount("/", routes![login::login_page, login::login, login::logout])
         .mount("/", StaticFiles::new("static", Options::Index))
         .register(catchers![not_found])
-        .launch();
+
+}
+
+fn main() {
+    rocket().launch();
 }
