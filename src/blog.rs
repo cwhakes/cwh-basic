9ac40e0bcdca20537ce2705b46f11df0377626ec
Will Hakes <info@cwilliamhakes.com>
2020-01-10 14:49:29 -0700

    Refactor blog

--- src/blog.rs
+++ src/blog.rs
@@ -6,32 +6,57 @@ use chrono::{DateTime, Utc};
 #[database("content_db")]
 pub struct ContentDb(postgres::Connection);
 
+#[derive(Debug)]
+struct BlogPost {
+    title: String,
+    date: DateTime<Utc>,
+    body: String,
+    prev_path:  Option<String>,
+    prev_title: Option<String>,
+    next_path:  Option<String>,
+    next_title: Option<String>,
+}
+
+impl ContentDb {
+    fn get_blog_post(&self, path: &str) -> Result<BlogPost, Status> {
+        let query_result = self.query(
+            "SELECT * FROM 
+                (SELECT *,
+                    LAG  (path,  1) OVER (ORDER BY ordinal) as prev_path,
+                    LAG  (title, 1) OVER (ORDER BY ordinal) as prev_title,
+                    LEAD (path,  1) OVER (ORDER BY ordinal) as next_path,
+                    LEAD (title, 1) OVER (ORDER BY ordinal) as next_title
+                FROM blog_posts)V
+            WHERE path = $1",
+            &[&path],
+        );
+
+        let blog_rows = query_result.or(Err(Status::InternalServerError))?;
+        let blog_row = blog_rows.iter().next().ok_or(Status::NotFound)?;
+
+        Ok(BlogPost {
+            title: blog_row.get_opt("title").map(Result::ok).flatten().ok_or(Status::InternalServerError)?,
+            date:  blog_row.get_opt("date") .map(Result::ok).flatten().ok_or(Status::InternalServerError)?,
+            body:  blog_row.get_opt("body") .map(Result::ok).flatten().ok_or(Status::InternalServerError)?,
+            prev_path:  blog_row.get_opt("prev_path").map(Result::ok).flatten().ok_or(Status::InternalServerError)?,
+            prev_title: blog_row.get_opt("prev_title").map(Result::ok).flatten().ok_or(Status::InternalServerError)?,
+            next_path:  blog_row.get_opt("next_path").map(Result::ok).flatten().ok_or(Status::InternalServerError)?,
+            next_title: blog_row.get_opt("next_title").map(Result::ok).flatten().ok_or(Status::InternalServerError)?,
+        })
+    }
+}
+
 #[get("/blog/<path>")]
 pub fn blog(content: ContentDb, path: String) -> Result<Template, Status> {
-    let query_result = content.query(
-        "SELECT * FROM 
-            (SELECT *,
-                LAG  (path,  1) OVER (ORDER BY ordinal) as prev_path,
-                LAG  (title, 1) OVER (ORDER BY ordinal) as prev_title,
-                LEAD (path,  1) OVER (ORDER BY ordinal) as next_path,
-                LEAD (title, 1) OVER (ORDER BY ordinal) as next_title
-            FROM blog_posts)V
-        WHERE path = $1",
-        &[&path],
-    );
-    
-    let blog_rows = query_result.or(Err(Status::InternalServerError))?;
-    let blog_row = blog_rows.iter().next().ok_or(Status::NotFound)?;
-
-    println!("{:?}", blog_row.columns());
+    let blog_post = content.get_blog_post(&path)?;
     
     let mut map = std::collections::HashMap::<&str, String>::new();
-    map.insert("title", blog_row.get("title"));
-    map.insert("date", blog_row.get::<&str, DateTime<Utc>>("date").to_rfc2822());
-    map.insert("body",  blog_row.get("body"));
-    map.insert("prev_path", blog_row.get_opt("prev_path").unwrap_or(Ok("".to_string())).or(Ok("".to_string()))?);
-    map.insert("prev_title", blog_row.get_opt("prev_title").unwrap_or(Ok("".to_string())).or(Ok("".to_string()))?);
-    map.insert("next_path", blog_row.get_opt("next_path").unwrap_or(Ok("".to_string())).or(Ok("".to_string()))?);
-    map.insert("next_title", blog_row.get_opt("next_title").unwrap_or(Ok("".to_string())).or(Ok("".to_string()))?);
+    map.insert("title", blog_post.title);
+    map.insert("date", blog_post.date.to_rfc2822());
+    map.insert("body",  blog_post.body);
+    map.insert("prev_path",  blog_post.prev_path.unwrap_or("".to_string()));
+    map.insert("prev_title", blog_post.prev_title.unwrap_or("".to_string()));
+    map.insert("next_path",  blog_post.next_path.unwrap_or("".to_string()));
+    map.insert("next_title", blog_post.next_title.unwrap_or("".to_string()));
     Ok(Template::render("blog", &map))
 }
